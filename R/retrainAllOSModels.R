591e765c94cbe4ab83eeebdd6b33af596b47c77a
AAMargolin <margolin@sagebase.org>
2012-10-04 05:52:30 +0000

    typo

--- R/retrainAllOSModels.R
+++ R/retrainAllOSModels.R
@@ -10,8 +10,8 @@ retrainAllOSModels <- function(){
   
   submittedModelIds_orig <- synapseQuery("SELECT id FROM entity WHERE entity.parentId == 'syn1125643'")
   
-  for (modelCtr in 1:nrow(submittedModelIds_orig){
-    curModelId_orig <- submittedModelIds_orig[modelCtr]
+  for (modelCtr in 1:nrow(submittedModelIds_orig)){
+    curModelId_orig <- submittedModelIds_orig$entity.id[modelCtr]
     print(paste("Retraining", modelCtr, "of", nrow(submittedModelIds_orig), ":", curModelId_orig))
     retrainOSModel(curModelId_orig)
   }
